9861c1ae713b6dcb925c93becc4acc9d6352c79a
avicentic <avicentic@gmail.com>
2018-09-15 14:07:00 +0200

    [NF] Implement validation.

--- rest-full-webservices/src/main/java/rs/avicentic/restfullwebservices/user/User.java
+++ rest-full-webservices/src/main/java/rs/avicentic/restfullwebservices/user/User.java
@@ -2,10 +2,17 @@ package rs.avicentic.restfullwebservices.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 
 	private Integer id;
+
+	@Size(min = 2, message = "Name should hve at least 2 characters")
 	private String name;
+
+	@Past
 	private Date birthDate;
 
 	protected User() {
