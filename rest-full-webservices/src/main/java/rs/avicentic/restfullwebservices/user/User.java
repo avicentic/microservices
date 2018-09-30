69f31093052012b588206a24a8c9632e78b6ae4b
avicentic <avicentic@gmail.com>
2018-09-30 21:34:19 +0200

    [NF] Configuring swagger.

--- rest-full-webservices/src/main/java/rs/avicentic/restfullwebservices/user/User.java
+++ rest-full-webservices/src/main/java/rs/avicentic/restfullwebservices/user/User.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,14 +16,17 @@ import lombok.ToString;
 @Setter
 @ToString
 @AllArgsConstructor
+@ApiModel(description = "All details about the user")
 public class User {
 
 	private Integer id;
 
 	@Size(min = 2, message = "Name should hve at least 2 characters")
+	@ApiModelProperty(notes = "Name should hve at least 2 characters")
 	private String name;
 
 	@Past
+	@ApiModelProperty(notes = "Birth date cannot be in the past")
 	private Date birthDate;
 
 	protected User() {
