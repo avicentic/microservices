ca18406cf642a5eda58aadbd0b2ebb8e4ba3c4fb
avicentic <avicentic@gmail.com>
2018-10-06 17:15:26 +0200

    [NF] Versioning in REST part 1.

---
+++ rest-full-webservices/src/main/java/rs/avicentic/restfullwebservices/versioning/PersonVersioningController.java
@@ -0,0 +1,20 @@
+package rs.avicentic.restfullwebservices.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonVersioningController {
+
+	@GetMapping("v1/person")
+	public PersonV1 personV1() {
+		return new PersonV1("John Smith");
+	}
+
+	@GetMapping("v2/person")
+	public PersonV2 personV2() {
+		return new PersonV2(new Name("John","Smith"));
+	}
+
+
+}
